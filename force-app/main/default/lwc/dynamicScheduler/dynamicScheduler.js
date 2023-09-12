a83b77b715400d8dd03ddb11cbea9cc0e07cb9fa
andresha20 <andreshenao.2021@gmail.com>
2023-09-12 16:23:45 -0500

    12/09/2023 - 16:23 - Eigth commit

--- force-app/main/default/lwc/dynamicScheduler/dynamicScheduler.js
+++ force-app/main/default/lwc/dynamicScheduler/dynamicScheduler.js
@@ -7,10 +7,12 @@ import Work_Order__c from '@salesforce/schema/Work_Order__c'
 import Schedule_Picker__c from '@salesforce/schema/Work_Order__c.Schedule_Picker__c'
 import Assistance_Type__c from '@salesforce/schema/Work_Order__c.Assistance_Type__c'
 import Schedule_Days_Picker__c from '@salesforce/schema/Work_Order__c.Schedule_Days_Picker__c'
+import updateTechniciansAvailability from '@salesforce/apex/DynamicShedulerTechsController.updateTechniciansAvailability';
 
 export default class DynamicScheduler extends LightningElement {
     @api recordId;
     formFields = {};
+    displayButton = false;
     assistanceTypeValues = [];
     daysPicklistValues = [];
     timePicklistValues = [];
@@ -66,15 +68,28 @@ export default class DynamicScheduler extends LightningElement {
         return this.showToast('Invalid or empty contact', error.body.message, 'error');
       }
       this.formFields['Technician__c'] = this.activeContact;
+      this.formFields['CreationDate'] = new Date().toISOString();
       const recordInput = { 
         apiName: Work_Order__c.objectApiName, 
+        
         fields: this.formFields
       };
       createRecord(recordInput).then(result => {
+        updateTechniciansAvailability({ 
+          technicianId: this.activeContact, 
+          scheduleDay: this.selectedDayValue, 
+          scheduleTime: this.selectedTimeValue 
+        }).then(result => {
+          console.log(`Contact record with ID ${result.Id}.`)
+        }).catch(error => {
+          console.log(error);
+          this.showToast('Error updating contact record', error.body.message, 'error');
+        })
         this.showToast('success', `Work order created with ID ${result.id}`);
         this.template.querySelector('form.createForm').reset();
         this.formFields = {};
       }).catch(error => {
+        console.log(error);
         this.showToast('Error creating record', error.body.message, 'error');
       })
     }
@@ -87,6 +102,7 @@ export default class DynamicScheduler extends LightningElement {
     }
     handleContactSelection(event) {
       this.activeContact = event.detail;
+      this.displayButton = true;
     }
     closeAction() {
       this.dispatchEvent(new CloseActionScreenEvent());
